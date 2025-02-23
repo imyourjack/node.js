f79d59ba2ba1e16778a8284522907d2c0a04e127
jhlee <dkdk9998@gmail.com>
2025-02-23 10:47:47 +0900

    html 파일 생성

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행 중')
 })
 
-app.get('/', (request, testMethod) => {
-    testMethod.send('TEST')
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
 })
 
-// git test
-// git test
-// git test
-// git test
-// git test
+app.get('/news', (request, response) => {
+    response.send('오늘 날씨 맑음')
+})
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/about.html')
+})
